f047e081e70584cc8d86387d1bc5e3b08ba879cb
Dustin Pfister <dustin.pfister@gmail.com>
2016-01-09 13:54:49 -0500

    using mongo now

--- server.js
+++ server.js
@@ -10,9 +10,22 @@ var express = require('express'),
 openShift = require('./lib/openshift.js').openShiftObj,
 expressLayouts = require('express-ejs-layouts'),
 
-
 // express app
 app = express();
+
+// mongoDB
+
+var mongoose = require('mongoose'),
+db = mongoose.createConnection(openShift.mongo),
+Schema = mongoose.Schema,
+
+// mongo example
+IPLOGGER = db.model('client', new Schema({
+
+    ip: String,
+    count: Number
+
+}));
 
 // app.use
 
@@ -21,6 +34,38 @@ app.set('view engine', 'ejs');
 app.use(expressLayouts);
 app.set('layout', 'layout_visit');  // default to layout_visit.ejs, not layout.ejs
 app.use(express.static('views')); // must do this to get external files
+
+app.get('*', function(req,res,next){
+
+
+    console.log(req.ip + ', ' + req.path);
+
+    IPLOGGER.findOne({'ip': req.ip }, '', function(err, IP){
+
+        if(!IP){
+
+            IP = new IPLOGGER({
+                ip : req.ip,
+                count : 1
+            });
+
+            IP.save(function(){
+                next();
+            });
+
+        }else{
+
+            IP.count += 1;
+            IP.save();
+            next();
+        }
+
+    });
+
+    
+
+});
+
 
 // paths
 app.get('/login', function(req, res){
