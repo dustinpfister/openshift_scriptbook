d5f676ca494148b1f43d059969b0269e2633e288
Dustin Pfister <dustin.pfister@gmail.com>
2016-01-09 00:57:23 -0500

    i hope this is it

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
 #!/bin/env node
 
+/*    OpenShift_demo by Dustin Pfister
+ *    https://github.com/dustinpfister/openshift_demo
+ *
+ *    A simple openshift.com demo app using express, and mongoose.
+ */
+
 var express = require('express'),
 openShift = require('./lib/openshift.js').openShiftObj,
 
-// express app
-app = express();
+    // express app
+    app = express();
+// trust proxy
+app.enable('trust proxy');
 
-app.get('/', function(req,res){
+// lets try EJS
+app.set('view engine', 'ejs');
+app.use(require('express-ejs-layouts'));
+app.use(express.static('views')); // must do this to get external files
 
-   res.send('scriptbook');
+// root path get requests
+app.get('/', function(req, res) {
+
+    res.send('scriptbook');
 
 });
 
-app.listen(openShift.port, openShift.host, function(){
-
-    console.log('i live');
-
-});
+// start server
+app.listen(openShift.port, openShift.ipaddress);
