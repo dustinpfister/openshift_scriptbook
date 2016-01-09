4d0cf375f6b28f0befedf74ad8b16ffddfdc0572
Dustin Pfister <dustin.pfister@gmail.com>
2016-01-09 11:45:31 -0500

    ejs

--- server.js
+++ server.js
@@ -8,16 +8,53 @@
 
 var express = require('express'),
 openShift = require('./lib/openshift.js').openShiftObj,
+expressLayouts = require('express-ejs-layouts'),
+
 
 // express app
 app = express();
 
+// app.use
+
+// use EJS for rendering
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.use(express.static('views')); // must do this to get external files
+
+
 // root path get requests
 app.get('/', function(req, res) {
 
-    res.send('scriptbook');
+    console.log(req.path);
+
+    //res.send('scriptbook');
+    res.render('root.ejs', {
+        data : {
+            time: new Date(),
+            activePath: req.path
+        }
+    });
+
+});
+
+// root path get requests
+app.get('/users', function(req, res) {
+
+    console.log(req.path);
+
+    //res.send('scriptbook');
+    res.render('users.ejs', {
+        data : {
+            time: new Date(),
+            activePath: req.path
+        }
+    });
 
 });
 
 // start server
-app.listen(openShift.port, openShift.ipaddress);
+app.listen(openShift.port, openShift.ipaddress, function(){
+
+    console.log('it lives');
+
+});
